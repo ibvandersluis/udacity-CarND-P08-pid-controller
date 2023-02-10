1abad444ca359e350e83e37fb2a34827da5d2b09
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2023-02-10 14:56:40 +0000

    Implement PID functions

--- src/pid_controller.cpp
+++ src/pid_controller.cpp
@@ -14,17 +14,19 @@ void PidController::init(double Kp, double Ki, double Kd)
   Kp_ = Kp;
   Ki_ = Ki;
   Kd_ = Kd;
+
+  p_error_ = 0.0;
+  i_error_ = 0.0;
+  d_error_ = 0.0;
 }
 
 void PidController::update_error(double cte)
 {
-  // TODO: Update PID errors based on cte.
+  d_error_ = cte - p_error_;
+  p_error_ = cte;
+  i_error_ += cte;
 }
 
-double PidController::total_error()
-{
-  // TODO: Calculate and return the total error
-  return 0.0;  // TODO: Add your total error calc here!
-}
+double PidController::total_error() { return -Kp_ * p_error_ - Ki_ * i_error_ - Kd_ * d_error_; }
 
 }  // namespace pid_controller
